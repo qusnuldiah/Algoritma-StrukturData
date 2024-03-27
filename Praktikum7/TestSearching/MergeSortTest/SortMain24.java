bb8ed5e14287126afeef12a6227c08fc7f63eb38
qusnuldiah <143980045+qusnuldiah@users.noreply.github.com>
2024-03-27 21:39:05 +0700

    Add files via upload

--- Praktikum7/TestSearching/MergeSortTest/SortMain24.java
+++ Praktikum7/TestSearching/MergeSortTest/SortMain24.java
@@ -1,1 +1,16 @@
+package MergeSortTest;
+
+public class SortMain24 {
+    public static void main(String[] args) {
+        int data[] = {10, 40, 30, 50, 70, 20, 100, 90};
+        System.out.println("Sorting dengan merge sort");
+        MergeSorting24 mSort = new MergeSorting24();
+        System.out.println("data awal");
+        mSort.printArray(data);
+        mSort.mergeSort(data);
+        System.out.println("Setelah diurutkan");
+        mSort.printArray(data);
+    }
+}
+
 

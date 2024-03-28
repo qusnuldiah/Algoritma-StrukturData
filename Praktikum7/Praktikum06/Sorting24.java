bcbc598bc77a568112a24a4f0675a7ae1dd99378
qusnuldiah <143980045+qusnuldiah@users.noreply.github.com>
2024-03-29 06:15:09 +0700

    Add files via upload

--- Praktikum7/Praktikum06/Sorting24.java
+++ Praktikum7/Praktikum06/Sorting24.java
@@ -1,75 +1,17 @@
-package minggu7;
-
-import java.util.Scanner;
-
-public class PencarianMhs24 {
-    Mahasiswa24 listMhs[];
-    int idx;
-
-    public PencarianMhs24(int jumlahMahasiswa) {
-        listMhs = new Mahasiswa24[jumlahMahasiswa];
-        idx = 0;
-    }
-
-    void tambah(Mahasiswa24 m) {
-        if (idx < listMhs.length) {
-            listMhs[idx] = m;
-            idx++;
-        } else {
-            System.out.println("Data sudah penuh !!");
-        }
-    }
-
-    void tampil() {
-        for (Mahasiswa24 m : listMhs) {
-            if (m != null) {
-                m.tampil();
-                System.out.println("--------------------");
+public class Sorting24 {
+    public static void sequentialSearch(int[] arr, int key) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == key) {
+                System.out.println("Data ditemukan pada indeks ke-" + i);
+                return;
             }
         }
+            System.out.println( "Data tidak ditemukan");
     }
 
-    public int findBinarySearchByName(String nama, int left, int right) {
-        int mid;
-        if (right >= left) {
-            mid = left + (right - left) / 2;
-            if (listMhs[mid] != null && listMhs[mid].nama.equalsIgnoreCase(nama)) {
-                return mid;
-            } else if (listMhs[mid] != null && listMhs[mid].nama.compareToIgnoreCase(nama) > 0) {
-                return findBinarySearchByName(nama, left, mid - 1);
-            } else {
-                return findBinarySearchByName(nama, mid + 1, right);
-            }
-        }
-        return -1;
-    }
-
-    public void searchAndPrint(String nama) {
-        int left = 0;
-        int right = idx - 1;
-        int pos = findBinarySearchByName(nama, left, right);
-        if (pos != -1) {
-            System.out.println("Data ditemukan pada indeks " + pos);
-            System.out.println("Detail Mahasiswa:");
-            listMhs[pos].tampil();
-        } else {
-            System.out.println("Data tidak ditemukan");
-        }
-    }
-
-    public void searchAllAndPrint(String nama) {
-        boolean found = false;
-        for (int i = 0; i < idx; i++) {
-            if (listMhs[i] != null && listMhs[i].nama.equalsIgnoreCase(nama)) {
-                if (!found) {
-                    found = true;
-                    System.out.println("Data ditemukan pada indeks:");
-                }
-                System.out.println(i);
-            }
-        }
-        if (!found) {
-            System.out.println("Data tidak ditemukan");
-        }
+    public static void main(String[] args) {
+        int[] daftarNilai = { 10, 5, 20, 15, 5, 45 };
+        sequentialSearch(daftarNilai, 5);
     }
 }
+
